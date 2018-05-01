ea0e66e488febc3c9124665d6a42c905afd1f819
Francisco Lucas <lucasalmeidacco@gmail.com>
2018-05-01 14:29:23 -0300

    Faznedo os primeiros testes e movimentacao

--- Xadrez/tabuleiro/Tabuleiro.cs
+++ Xadrez/tabuleiro/Tabuleiro.cs
@@ -46,7 +46,17 @@ namespace tabuleiro
             p.posicao = pos;
         }
 
-
+        public Peca retirarPeca(Posicao pos)
+        {
+            if (peca(pos) == null)
+            {
+                return null;
+            }
+            Peca aux = peca(pos);
+            aux.posicao = null;
+            pecas[pos.linha, pos.coluna] = null;
+            return aux;
+        }
 
 
 // Validação de Posição
